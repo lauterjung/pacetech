d35fe5e5a10b606a468af0f94689d68650ed2387
lauterjung <miguelbusarello@hotmail.com>
2022-03-14 13:15:08 -0300

    Adicionado projeto da semana 6

---
+++ 6_ConversorEua/src/conversorEua/Menu.java
@@ -0,0 +1,97 @@
+package conversorEua;
+
+import java.util.Scanner;
+
+public class Menu {
+    static Scanner scan = new Scanner(System.in);
+    
+    public void open() {
+        System.out.println("""
+Operações de Conversão
+		
+Temperatura
+(a) Celsius (°C) para Fahrenheit (°F)
+(b) Fahrenheit (°F) para Celsius (°C)
+		
+Moeda
+(c) Real para Dólar
+(d) Dólar para Real
+		
+Outros
+(q) Sair
+		
+Digite a tecla correspondente de qual operação você deseja realizar""");
+        
+        String userInput = scan.next().toLowerCase();
+                
+        switch(userInput){
+        case "a" -> celsiusToFahrenheit();
+        case "b" -> fahrenheitToCelsius();
+        case "c" -> realToDolar();
+        case "d" -> dolarToReal();
+        case "q" -> {
+            System.out.println("Boa estadia!");
+			scan.close();
+            System.exit(0);
+            }
+        default -> {
+        	System.out.println("Não entendi a operação.");              
+        }
+		}
+  
+		newOperation();
+	}
+
+    public void newOperation() {
+        String newOperation;
+        do {
+            System.out.println("\nDeseja fazer nova operação? (s/n)");
+            newOperation = scan.next().toLowerCase();
+        } while (!newOperation.equals("s") && !newOperation.equals("n"));
+
+        if (newOperation.equals("s")) {
+            open();
+        } else if (newOperation.equals("n")) {
+            System.out.println("Boa estadia!");
+            scan.close();
+            System.exit(0);
+        }
+    }
+
+    public void celsiusToFahrenheit() {
+        System.out.println("Informe a temperatura (°C):");
+        double tempCelsius = scan.nextDouble();
+        double tempFahrenheit = tempCelsius*1.8+32;
+        System.out.printf("%.2f °C equivalem à %.2f °F", tempCelsius, tempFahrenheit);
+    }
+    
+    public void fahrenheitToCelsius() {
+        System.out.println("Informe a temperatura (°F):");
+        double tempFahrenheit = scan.nextDouble();
+        double tempCelsius = (tempFahrenheit-32)/1.8;
+        System.out.printf("%.2f °F equivalem à %.2f °C", tempFahrenheit, tempCelsius);
+    }
+    
+    public void realToDolar() {
+        System.out.println("Informe a cotação do dólar:");
+        double cotacaoDolar = scan.nextDouble();
+        System.out.println("Informe o valor (em reais) a ser convertido:");
+        double valorReal = scan.nextDouble();
+        double valorDolar = valorReal/cotacaoDolar;
+        System.out.printf("%.2f reais equivalem à %.2f dólares", valorReal, valorDolar);
+    }
+    
+    public void dolarToReal() {
+        System.out.println("Informe a cotação do dólar:");
+        double cotacaoDolar = scan.nextDouble();      
+        System.out.println("Informe o valor (em dólares) a ser convertido:");
+        double valorDolar = scan.nextDouble();
+        double valorReal = valorDolar*cotacaoDolar;
+        System.out.printf("%.2f dólares equivalem à %.2f reais", valorDolar, valorReal);
+    }
+    
+    public void main(String[] args) {
+           open();
+    }
+}
+
---
+++ 6_ConversorEua/src/conversorEua/main.java
@@ -0,0 +1,12 @@
+// Tive problemas com o Scanner no NetBeans (algo relacionado ao JDK).
+// O programa foi feito na IDE Eclipse.
+package conversorEua;
+
+public class main {
+
+    public static void main(String[] args) {
+    	Menu menu = new Menu();
+        menu.open(); // arquivo Menu.java
+    }
+}
+
