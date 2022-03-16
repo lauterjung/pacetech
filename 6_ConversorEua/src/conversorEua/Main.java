6efbe57a74db8429847d2dcb5d914c592139a7aa
lauterjung <miguelbusarello@hotmail.com>
2022-03-15 21:34:58 -0300

    Ajustes

--- 6_ConversorEua/src/conversorEua/Main.java
+++ 6_ConversorEua/src/conversorEua/Main.java
@@ -2,7 +2,7 @@
 // O programa foi feito na IDE Eclipse.
 package conversorEua;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
     	Menu menu = new Menu();
